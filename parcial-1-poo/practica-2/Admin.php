ebf9ee097bc3825c221bfc98c81957678c904f67
hectorrubio11 <hectorrubaya76@gmail.com>
2026-03-07 09:03:35 -0700

    correcciones

--- parcial-1-poo/practica-2/Admin.php
+++ parcial-1-poo/practica-2/Admin.php
@@ -1,8 +1,11 @@
 <?php
 
 include "Usuario.php";
+
+//extiende la clase Usuario
 class Admin extends Usuario{
     public function getRol(){
+        //solo imprime Administrador
         return "Administrador";
     }
 }
