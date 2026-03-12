7ebd5a2e0f3889aaa97b4c48abba7b0a82404181
hectorrubio11 <hectorrubaya76@gmail.com>
2026-03-11 17:00:41 -0700

    comentar clases

--- parcial-1-poo/examen-practico/Usuario.php
+++ parcial-1-poo/examen-practico/Usuario.php
@@ -6,13 +6,25 @@
         public function __construct($nombre,$correo){
             $this->nombre = $nombre;
             $this->correo = $correo;
+
+            /**
+             * @throws Exception
+             */
             if (!filter_var($correo,FILTER_VALIDATE_EMAIL)){
                 throw new Exception("Excepción controlada: correo inválido");
             }
         }
+        /**
+         *
+         * @return string
+         */
         public function getNombre(){
             return $this->nombre;
         }
+        /**
+         *
+         * @return string
+         */
         public function getCorreo(){
             return $this->correo;
         }
